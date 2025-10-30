38e039793324c99f728dc28120271bf1cfc7674e
mmartin1995 <molnmartin.1995@gmail.com>
2025-10-30 09:28:01 +0100

    visszaszamlalo

--- visszaszamlalo/app.py
+++ visszaszamlalo/app.py
@@ -1,14 +1,14 @@
-# C:\MyProjects\molnarmartinzoltan\valasztasvisszaszamlalo\app.py
+# C:\MyProjects\molnarmartinzoltan\visszaszamlalo\app.py
 from flask import Blueprint, render_template
 from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
-valasztas_blueprint = Blueprint("visszaszamlalo", __name__)
+visszaszamlalo_blueprint = Blueprint("visszaszamlalo", __name__)
 
 TARGET_TZ = ZoneInfo("Europe/Budapest")
 TARGET_DT = datetime(2026, 4, 12, 6, 0, 0, tzinfo=TARGET_TZ)
 
-@valasztas_blueprint.route("/")
+@visszaszamlalo_blueprint.route("/")
 def index():
     target_epoch_ms = int(TARGET_DT.timestamp() * 1000)
     server_now_ms = int(datetime.now(timezone.utc).timestamp() * 1000)
